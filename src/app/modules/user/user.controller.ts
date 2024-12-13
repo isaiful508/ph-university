c5aeef846286993e2403b92ffa85f2b3db46fa7b
Md. Saiful islam <isaiful.islam508@gmail.com>
2024-12-13 08:29:14 +0600

    global error handler add androutes add

---
+++ src/app/modules/user/user.controller.ts
@@ -0,0 +1,34 @@
+import { StatusCodes } from 'http-status-codes';
+import { NextFunction, Request, Response } from 'express';
+import sendResponse from '../../../utils/sendResponse';
+import { UserServices } from './user.service';
+
+const createStudent = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const { password, student: studentData } = req.body;
+
+    // const zodParsedData = studentValidationSchema.parse(studentData);
+
+    const result = await UserServices.createStudentIntoDB(
+      password,
+      studentData,
+    );
+
+    sendResponse(res, {
+      statusCode: StatusCodes.OK,
+      success: true,
+      message: 'Student is created succesfully',
+      data: result,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const UserControllers = {
+  createStudent,
+};
